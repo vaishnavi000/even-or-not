1bb845fbd939e8d720766f81f2dde70f214ecccc
vaishnavi000 <98035398+vaishnavi000@users.noreply.github.com>
2022-01-20 21:18:23 +0530

    Add files via upload

---
+++ q6a.java
@@ -0,0 +1,12 @@
+package test_project;
+public class q6a{
+	public static void main(String args[]) {
+		int i=0,j=0;
+		for(i=0;i<4;i++) {
+			for(j=0;j<=i;j++) {
+				System.out.print("* ");
+			}
+			System.out.println("");
+		}
+	}
+}
